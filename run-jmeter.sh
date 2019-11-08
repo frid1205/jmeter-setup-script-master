c5be99f1b311d807b65d458dd422063b3def4e45
frid1205 <33950395+frid1205@users.noreply.github.com>
2019-11-08 17:49:25 +0700

    Update run-jmeter.sh

--- run-jmeter.sh
+++ run-jmeter.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
-var1=$(docker inspect $(docker ps -a | grep -v "master" | awk 'NR>1 {print $1}' ) | grep -Po '"IPAddress":.*?[^\$
+var1=$(docker inspect $(docker ps -a | grep -v "master" | awk 'NR>1 {print $1}'
+ ) | grep -Po '"IPAddress":.*?[^\\]?[^\\]",' |awk '{print $2}'| sort -u | tr -d '"'| tr -d '\n')
 
-docker exec -it master /bin/bash -c "rm -rf $1 $2 $3"
 
-docker cp /home/ubuntu/$1 master:$1
-docker exec -it master /bin/bash -c "jmeter -n -t /$1 -R${var1::-1} -l /$2 -e -o $3"
+docker exec -it master /bin/bash -c "rm -rf $1 $2"
 
-wait 
-docker cp master:$2 /home/ubuntu/$2
-docker cp master:$3 /home/ubuntu/$3
+docker cp /home/fendy_ridwan/$1 master:$1
+
+docker exec -it master /bin/bash -c "rm -rf $1 $2"
+
+docker cp /home/fendy_ridwan master:$1
+
+docker cp /home/fendy_ridwan/latihan.jmx master:/HTTPReq.jmx
+
+docker exec -it master /bin/bash -c "jmeter -n -t /HTTPReq.jmx -R${var1::-1} -l /testresult.jtl"
